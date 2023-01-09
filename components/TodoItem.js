bc90991d49cf75a4da9707d0b82d5cf0394c4c73
InSeong-So <goflvhxj2547@gmail.com>
2023-01-09 17:54:10 +0900

    Feat: Pressable을 활용한 UI 피드백 추가

--- components/TodoItem.js
+++ components/TodoItem.js
@@ -2,22 +2,29 @@ import { StyleSheet, Text, View, Pressable } from 'react-native';
 
 export default function TodoItem({ id, text, onDeleteTodo }) {
   return (
-    <Pressable onPress={onDeleteTodo.bind(this, id)}>
-      <View style={styles.todoItem}>
+    <View style={styles.todoItem}>
+      <Pressable
+        android_ripple={{ color: '#4b08a2' }}
+        onPress={onDeleteTodo.bind(this, id)}
+        style={({ pressed }) => pressed && styles.pressedItem}
+      >
         <Text style={styles.todoText}>{text}</Text>
-      </View>
-    </Pressable>
+      </Pressable>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   todoItem: {
     margin: 8,
-    padding: 8,
     borderRadius: 6,
     backgroundColor: '#5e0acc',
   },
+  pressedItem: {
+    opacity: 0.5,
+  },
   todoText: {
     color: 'white',
+    padding: 8,
   },
 });
